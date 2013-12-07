61c447bfcb733cb61074b8827e69127a064da612
Arthur Corenzan <arthur@corenzan.com>
2013-12-07 13:37:18 -0200

    Fixed wrong command for count of commits

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
 module Nait
-  VERSION = "#{`git rev-parse HEAD`[0..5]} R#{`git rev-parse HEAD --count`}"
+  VERSION = "#{`git rev-parse HEAD`[0..5]} R#{`git rev-list HEAD --count`}"
   VERSION = ENV.fetch('VERSION', VERSION)
 end
