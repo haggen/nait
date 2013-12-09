bca882e94e6a29a088fa39e852f39a1f51c07994
Arthur Corenzan <arthur@corenzan.com>
2013-12-09 12:38:29 -0200

    Now remote links will start loading animation

--- app/assets/javascripts/application/remote.js
+++ app/assets/javascripts/application/remote.js
@@ -1,3 +1,7 @@
-$(document).on('ajax:complete', 'a[data-remote]', function() {
+$(document).on('ajax:loading', 'a[data-remote], form[data-remote]', function() {
+  NProgress.start();
+});
+
+$(document).on('ajax:complete', 'a[data-remote], form[data-remote]', function() {
   Turbolinks.visit(location.pathname);
 });
