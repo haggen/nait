b6e7691fd1f7a6f19ffd59d723281e8f1523b538
Arthur Corenzan <arthur@corenzan.com>
2013-12-09 11:03:19 -0200

    Fix constant warning

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
 module Nait
-  VERSION = "#{`git rev-parse HEAD`[0..5]} R#{`git rev-list HEAD --count`}"
-  VERSION = ENV.fetch('VERSION', VERSION)
+  version = "#{`git rev-parse HEAD`[0..5]} R#{`git rev-list HEAD --count`}"
+  VERSION = ENV.fetch('VERSION', version)
 end
