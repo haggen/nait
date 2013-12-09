d59636b7aba6fad4a2bb6ccda9c93a999bac0dce
Arthur Corenzan <arthur@corenzan.com>
2013-12-09 12:41:40 -0200

    Fixed which event to start loading animation

--- app/assets/javascripts/application/remote.js
+++ app/assets/javascripts/application/remote.js
@@ -1,7 +1,7 @@
-$(document).on('ajax:loading', 'a[data-remote], form[data-remote]', function() {
+$(document).on('click', 'a[data-remote]', function() {
   NProgress.start();
 });
 
-$(document).on('ajax:complete', 'a[data-remote], form[data-remote]', function() {
+$(document).on('ajax:complete', 'a[data-remote]', function() {
   Turbolinks.visit(location.pathname);
 });
