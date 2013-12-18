e31b6ab3544f3204766361d615210b3cd44d0b5d
Arthur Corenzan <arthur@corenzan.com>
2013-12-18 19:19:22 -0200

    Accepts nested attributes for user

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,17 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  accepts_nested_attributes_for :user
+  validates_presence_of :project
+  validates_presence_of :user
+
+  validates_associated :user, :if => :user_id_blank?
+  accepts_nested_attributes_for :user, :reject_if => :user_id_not_blank?
+
+  def user_id_blank?
+    self.user_id.blank?
+  end
+
+  def user_id_not_blank?
+    not user_id_blank?
+  end
 end
